ad2c266732e366943ef4045c073e4a1052c8e522
KevinMarais <kevin.n.marais@gmail.com>
2015-01-24 23:27:57 +0200

    Menu Screen

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float Speed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //print(this.GetComponent<Material>().mainTextureOffset);
+        renderer.material.mainTextureOffset = Speed * Vector3.down;
+        //Speed++;
+    }
+}
