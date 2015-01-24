da92916b60c5a4fcb5a1ed1625287537fbc313ce
KevinMarais <kevin.n.marais@gmail.com>
2015-01-24 07:49:00 +0200

    enemy project

---
+++ Assets/Scripts/EnemyProjectileScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyProjectileScript : MonoBehaviour {
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            GameObject.Find(coll.name + "Health").GetComponent<Text>().text = coll.name + ": " + (coll.GetComponent<PlayerStats>().Health -= 50);
+
+        }
+    }
+}
