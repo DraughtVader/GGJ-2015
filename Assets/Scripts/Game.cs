6c213645e42a990a2e99bf7122f56dee7fe89f43
Cukia Kimani <cukiakimani@gmail.com>
2015-01-25 13:23:56 +0200

    - All sorts

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,13 +7,15 @@ public class Game : MonoBehaviour  {
 
     void Update()
     {
+        
         if (!GameOver)
             return;
 
         if (Input.GetAxisRaw("Action1") > 0 || Input.GetAxisRaw("Action2") > 0)
         {
             GameOver = false;
-            Application.LoadLevel(Application.loadedLevel);
+            Time.timeScale = 1f;
+            Application.LoadLevel(1);
         }
     }
 }
