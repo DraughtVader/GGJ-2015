f0c4bb5ea1c7fe677b1277846162cfe301fb4812
Cukia Kimani <cukiakimani@gmail.com>
2015-01-25 11:12:04 +0200

    gameover method

--- Assets/Scripts/GunMovement.cs
+++ Assets/Scripts/GunMovement.cs
@@ -53,6 +53,9 @@ public class GunMovement : MonoBehaviour
         }
         else if (coll.gameObject.tag == "SideBounds") 
         {
+            GameObject.Find("GameOverText").GetComponent<Animator>().SetTrigger("GameOver");
+            GameObject.Find("DistanceRecord").GetComponent<Text>().text = 
+            GameObject.Find("DistanceRecordShadow").GetComponent<Text>().text = "Distance: " + DistanceTracker.CurrentDistance;
             Time.timeScale = 0;
             GameObject.Find("GameOverText").GetComponent<Text>().text = "GAME OVER";
             Game.GameOver = true;
