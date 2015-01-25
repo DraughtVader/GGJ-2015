ce86f6a7d2bc3aa84c096b44ea84942f5de76c85
KevinMarais <kevin.n.marais@gmail.com>
2015-01-25 05:49:49 +0200

    Field Tiling

---
+++ Assets/Scripts/FieldManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FieldManager : MonoBehaviour
+{
+    List<Transform> Fields;
+
+    void Start()
+    {
+        GameObject[] fieldObjs = GameObject.FindGameObjectsWithTag("Field");
+        Fields = new List<Transform>();
+        foreach (var f in fieldObjs)
+        {
+            Fields.Add(f.transform);
+        }
+    }
+
+    void Update()
+    {
+        Vector3 top = this.transform.position + 384 * Vector3.up;
+        Vector3 cameraBottom = Camera.main.transform.position + 864 * Vector3.down;
+        Vector3 topMostField = Vector3.zero;
+            
+        foreach (var f in Fields)
+        {
+            if (f.position.y > topMostField.y)
+            {
+                topMostField = f.position;
+            }
+        }
+
+        if (top.y < cameraBottom.y)
+        {
+            this.transform.position = topMostField + 768 * Vector3.up;
+        }
+    }
+}
